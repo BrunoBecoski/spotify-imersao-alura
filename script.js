86fadbca3b2eb4c78e803d79e45970b8828cf624
Bruno Becoski <brunobecoski@gmail.com>
2025-01-31 12:21:00 -0300

    Aula 4

---
+++ script.js
@@ -0,0 +1,37 @@
+const searchInput = document.getElementById('search-input');
+const resultsArtist = document.getElementById('result-artist');
+const resultPlaylists = document.getElementById('result-playlists')
+
+searchInput.addEventListener('input', () => {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  if (searchTerm === '') {
+    resultPlaylists.classList.add('hidden');
+    resultsArtist.classList.remove('hidden');
+    return
+  }
+
+  requestApi(searchTerm)
+})
+
+function requestApi(searchTerm) {
+  const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+  resultPlaylists.classList.add('hidden');
+  
+  const artistName = document.getElementById('artist-name');
+  const artistImg = document.getElementById('artist-img');
+
+  result.forEach(element => {
+    artistName.innerText = element.name
+    artistImg.src = element.urlImg
+  });
+
+  resultsArtist.classList.remove('hidden')
+}
